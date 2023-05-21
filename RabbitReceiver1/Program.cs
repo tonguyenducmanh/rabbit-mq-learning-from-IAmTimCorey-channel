b6cc3197f30be73b6612cfdc18614442b2b2d59b
to nguyen duc manh <ducmanh14032000@gmail.com>
2023-05-21 19:28:48 +0700

    Add project files.

---
+++ RabbitReceiver1/Program.cs
@@ -0,0 +1,72 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//ứng dụng receiver thứ nhất ( vào trong rabit mq đọc message)
+//created by tdmanh1 21/05/2023
+//test tạo ra ứng dụng gửi và nhận message cho rabbitmq
+
+int _timeWait = 2;
+
+// tạo ra factory
+ConnectionFactory factory = new ConnectionFactory();
+// khai báo tài khoản, mật khẩu và địa chỉ của rabbitmq ( được tạo ra theo file readme.md )
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+// khai báo tên dùng trong application console này
+// vì sau có thể có nhiều app sender dùng tạo queue, dùng để phân biệt
+factory.ClientProvidedName = "Rabbit Receiver1 App";
+
+// tạo kết nối
+IConnection cnn = factory.CreateConnection();
+
+// tạo ra model
+IModel channel = cnn.CreateModel();
+
+// exchange, channel, queue là 3 tab ở trong localhost:8080 (rabbitmq) sau khi đăng nhập thành công
+
+// cấu hình 3 trường bên dưới phải giống bên project RabbitSender
+string exchangeName = "manh-DemoExchange";
+string routingKey = "manh-demo-routing-key";
+string queueName = "manh-DemoQueue";
+
+// gán 3 biến trên vào chanel object
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+channel.QueueDeclare(queueName, false, false, false, null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+// thiết lập Quality of Services
+// prefetchSize = 0 : không quan tâm size của message
+// prefetchCount = 1 : chỉ muốn nhận 1 message 1 thời điểm
+// global : chỉ áp dụng cho instance hiện tại
+channel.BasicQos(0,1,false);
+Console.OutputEncoding = Encoding.UTF8;
+var consumer  = new EventingBasicConsumer(channel);
+
+// nhận từng message về 1 lần
+consumer.Received += (sender, args) =>
+{
+    // dừng 2 giây giả lập tải message từ internet về
+    Task.Delay(TimeSpan.FromSeconds(_timeWait)).Wait(); 
+
+    var body = args.Body.ToArray();
+
+    // biến các bytes[] được tạo ra từ message bên sender thành string đọc được
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Message nhận được từ Rabbit MQ: {message}");
+
+    // loại trừ delivery tag
+    channel.BasicAck(args.DeliveryTag, false);
+};
+
+string consumerTag = channel.BasicConsume(queueName, false, consumer);
+
+
+// phải có dòng này vì là console app, không có console app mặc định chạy xong sẽ đóng
+// thêm vào để enter để đóng
+Console.ReadLine();
+
+// xóa message đã đọc đi
+channel.BasicCancel(consumerTag);
+
+channel.Close();
+cnn.Close();
---
+++ RabbitSender/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+//ứng dụng sender ( gửi message vào trong rabit mq )
+//created by tdmanh1 21/05/2023
+//test tạo ra ứng dụng gửi và nhận message cho rabbitmq
+
+int _countSending = 30;
+int _timeWait = 1000;
+// tạo ra factory
+ConnectionFactory factory  = new ConnectionFactory();
+// khai báo tài khoản, mật khẩu và địa chỉ của rabbitmq ( được tạo ra theo file readme.md )
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+// khai báo tên dùng trong application console này
+// vì sau có thể có nhiều app sender dùng tạo queue, dùng để phân biệt
+factory.ClientProvidedName = "Rabbit Sender App";
+
+// tạo kết nối
+IConnection cnn = factory.CreateConnection();
+
+// tạo ra model
+IModel channel =  cnn.CreateModel();
+
+// exchange, channel, queue là 3 tab ở trong localhost:8080 (rabbitmq) sau khi đăng nhập thành công
+string exchangeName = "manh-DemoExchange";
+string routingKey = "manh-demo-routing-key";
+string queueName = "manh-DemoQueue";
+
+// gán 3 biến trên vào chanel object
+channel.ExchangeDeclare(exchangeName, ExchangeType.Direct); 
+channel.QueueDeclare(queueName, false, false, false,null);
+channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+// tạo ra vòng lặp gửi lên rabbit mq giả lập gửi đi nhiều lần
+for(int i = 0;i < _countSending; i++)
+{
+    // gửi message vào queue, phải biến nó thành kiểu byte
+    byte[] messageBodyBytes = Encoding.UTF8.GetBytes($"Hello World from Rabbit MQ lần {i +1}, thời gian gửi {DateTime.Now}");
+
+    // đẩy 1 message lên channel này
+    channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+    // dừng 2 giây giả lập tải message lên
+    Thread.Sleep(_timeWait);
+}
+
+// đóng kết nối sau khi đẩy 1 message lên thành công
+channel.Close();
